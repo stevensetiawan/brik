9a3e59b99baf6003ee2a6c5477d47a222c3adc44
stevensetiawan <steven.setiawan.94@gmail.com>
2023-09-19 13:04:22 +0700

    clean code

--- FE/src/utils/authentication/sign.js
+++ FE/src/utils/authentication/sign.js
@@ -6,7 +6,6 @@ export const signingIn = async (email, password) => {
   }
   return await axios.post('http://localhost:7009/api/v1/user/login', payload)
     .then(response => {
-      console.log(response,"ini response")
       return response
     })
     .catch(error => {
--- FE/src/utils/authentication/signOut.js
+++ FE/src/utils/authentication/signOut.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 export const signingOut = async () => {
   return await axios.post('http://localhost:7009/api/v1/user/logout')
     .then(response => {
-      console.log(response,"ini response")
       return response
     })
     .catch(error => {
